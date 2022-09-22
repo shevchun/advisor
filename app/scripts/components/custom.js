af88cb9f20e8372395cbd95e11c1b45464891e9a
Vladimir Shevchun <vovashevchun@gmail.com>
2022-09-22 22:26:51 +0200

    home

--- app/scripts/components/custom.js
+++ app/scripts/components/custom.js
@@ -1,6 +1,21 @@
 import $ from 'jquery'
 
 export default () => {
+
+  $('[data-rate]').each((index, element) => {
+    const val = $(element).attr('data-rate')
+    const full = val.split('.')[0] || null
+    const fraction = val.split('.')[1] || null
+    $(element).find($('.rate__item-holder-inner')).each((i, item) => {
+      if (i < +full) {
+        $(item).addClass('active')
+      }
+      if ((i === +full) && fraction) {
+        $(item).addClass('active').css('width', `${fraction}0%`)
+      }
+    })
+  })
+
   $(window).scroll(function scroll() {
     const heightWindow = 50
     if ($(this).scrollTop() >= heightWindow) {
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,12 +4,14 @@ import svg4everybody from 'svg4everybody'
 
 import custom from './components/custom'
 import sliders from './components/sliders'
+import Tabs from './components/tabs'
 
 global.$ = $
 global.jQuery = $
 
 document.addEventListener('DOMContentLoaded', () => {
   svg4everybody()
+  global.Tabs = new Tabs()
   sliders()
   custom()
 })
